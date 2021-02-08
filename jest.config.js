23960c0cbe3a8e941076ded866e505248c4a3c68
Matheus Henrique <matheus.181137@gmail.com>
2021-02-07 22:07:59 -0400

    refactor: changed jest configurations

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
-  ],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect']
 }
